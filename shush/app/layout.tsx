7f157c819f5e2718cff9265e240bde66a1e58d58
arihanv <arihanvaranasi@gmail.com>
2023-11-20 18:18:02 -0600

    fix: add toaster

--- shush/app/layout.tsx
+++ shush/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { Toaster } from 'sonner'
 
 export const metadata: Metadata = {
   title: "Shush",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={GeistSans.className}>
+        <Toaster />
         <Navbar/>
         {children}
       </body>
