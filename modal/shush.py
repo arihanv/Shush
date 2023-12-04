8cf27c68a157bd33424f9ba16499aaeb3d70826e
Akshat Bubna <akshatb42@gmail.com>
2023-12-04 00:43:52 +0000

    add CORS policy to shush.py

--- modal/shush.py
+++ modal/shush.py
@@ -9,11 +9,21 @@ from modal import (
     functions,
 )
 from fastapi import Request, FastAPI, responses
+from fastapi.middleware.cors import CORSMiddleware
 import tempfile
 
 MODEL_DIR = "/model"
 
 web_app = FastAPI()
+
+
+web_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def download_model():
@@ -126,7 +136,7 @@ async def get_completion(request: Request):
     return result
 
 
-@stub.function(allow_concurrent_inputs=80)
+@stub.function(allow_concurrent_inputs=4)
 @asgi_app()
 def entrypoint():
     return web_app
